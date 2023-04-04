cecf16b32f4a41248d73a39b853e58c9c0af6e87
LautaroRojasTorrilla <lautaro.rojas.torrilla@gmail.com>
2023-04-04 13:57:44 -0300

    creacion superuser y revision admin

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from inicio.models import Animal, Persona
 
 # Register your models here.
+
+# admin.site.register(Animal)
+# admin.site.register(Persona)
+admin.site.register([Animal, Persona])
